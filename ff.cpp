60be190a6d55b6ead152f925b9bdbb1a690cac49
artem-sergiyovych <artem-sergiyovych@users.noreply.github.com>
2022-03-23 21:48:41 +0100

    kehrghk

---
+++ ff.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Artem Tymoshchuk on 17.03.2022.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello world"<<endl;
+    return 0;
+}
+
--- main.cpp
+++ main.cpp
@@ -106,12 +106,104 @@ public:
         return alnum;
     }
 };
-    int main() {
-        Student student;
-        student.inputation();
-        student.add_new_grades();
-        student.display_summary();
-        student.mean_grade();
-        student.cheaker_of_2();
-        student.cheaker_of_valid_alnum();
+
+
+//2nd exercise
+class Complex
+{
+    float real_p;
+    float imag_p;
+
+public:
+
+    Complex()
+    {
+        real_p = 0;
+        imag_p = 0;
+    }
+
+    Complex(float real, float imaginary)
+    {
+        real_p = real;
+        imag_p = imaginary;
+    }
+
+    Complex(float real)
+    {
+        real_p = real;
+        imag_p = 0;
+    }
+
+    void modify_num()
+    {
+        cout << "Enter real part:" << endl;
+        cin >> real_p;
+        cout << "Enter imaginary part:" << endl;
+        cin >> imag_p;
+    }
+
+    Complex add_num(Complex &b)
+    {
+        Complex num;
+        num.real_p = real_p;
+        num.imag_p = imag_p;
+        num.real_p += b.real_p;
+        num.imag_p += b.imag_p;
+        return num;
+    }
+
+    void print()
+    {
+        if (imag_p < 0)
+        {
+            cout << real_p << " - " << abs(imag_p) << "i" << endl;
+        }
+        else if (imag_p > 0)
+        {
+            cout << real_p << " + " << imag_p << "i" << endl;
+        }
+        else
+        {
+            cout << real_p;
+        }
+    }
+
+    void set_im(float num)
+    {
+        imag_p = num;
+    }
+};
+
+int main()
+{
+    char choice;
+    cout << "Choose the option:\n"
+            "1 - First part\n"
+            "2 - Second part" << endl;
+    choice = getchar();
+    switch(choice)
+    {
+        case '1':
+        {
+            Student student;
+            student.inputation();
+            student.add_new_grades();
+            student.display_summary();
+            student.mean_grade();
+            student.cheaker_of_2();
+            student.cheaker_of_valid_alnum();
+            break;
+        }
+        case '2':
+        {
+            Complex complex_n(1.0, -2.0);
+            Complex complex_pi(3.14);
+
+            complex_pi.set_im(-5);
+
+            Complex c = complex_n.add_num(complex_pi);
+
+            c.print();
+        }
+    }
 }
